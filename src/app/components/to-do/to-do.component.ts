440067dbc4e7d5539ce546409e122462830e5fd6
andresfelipemuñoz <andres.munozc@opitech.com.co>
2024-05-16 23:31:53 -0500

    diseño1 task

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -33,49 +33,6 @@ export class ToDoComponent {
         alert("Task updated")
       })
     }
-    // this._tasksService.updateTask(Update).subscribe( (data) => {
-    //     if (task.isState === 0) {
-    //       task.isState = 1;
-    //       data=task.isState ;
-    //     } else {
-    //       console.error('Respuesta del servidor no válida', task);
-    //     }
-      
-      
-    // });
   }
-  // editTask(task: ITaskOpiModel): void {
-  //   const taskId = task.id; 
-  //   this._tasksService.updateTask(task).subscribe({
-  //     next: (data) => {
-  //       // Verificar si el estado ha cambiado
-  //       if (data.isState !== task) {
-  //         // Actualizar el estado de la tarea
-  //         task.isState = data.isState;
-  //       }
-  //       console.log('Nuevo estado de la tarea:', task.isState);
-  //     },
-  //     error: (err) => {
-  //       console.error('Error al actualizar la tarea:', err);
-  //     }
-  //   });
-  // }
-  // editTask(task:any):void{
-
-  //   this._tasksService.updateTask(task).subscribe(data=>{
-  //     console.log(!data.isState)
-  //     if(data.isState === task.isState)
-  //       {
-        
-  //         console.log(!data.isState)
-  //         return !data.isState
-  //       }
-  //       console.log(data.isState)
-  //       return data.isState
-  //      });
-       
-    
-
-  //  }
 
 }
